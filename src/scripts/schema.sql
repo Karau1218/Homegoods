a5ab8756a9b7b5e3b252ce090ee2b95b3e5d355c
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-02-24 12:41:05 -0800

    catagory added to the table

--- src/scripts/schema.sql
+++ src/scripts/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE products (
     productId INT AUTO_INCREMENT PRIMARY KEY,
     productName VARCHAR(150) NOT NULL,
     productDescription TEXT NOT NULL,
-    price DECIMAL(10,2) NOT NULL
+    price DECIMAL(10,2) NOT NULL,
+    category VARCHAR(100) NOT NULL
 );
