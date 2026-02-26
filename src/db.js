74eae0b65516a30dffa214d6e24e72e633b1e831
Lilian <210286008+liliann19@users.noreply.github.com>
2026-02-26 12:25:38 -0800

    fixed database connection

--- src/db.js
+++ src/db.js
@@ -1,10 +1,16 @@
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_PASSWORD, DB_DATABASE, DB_USER } = process.env;
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",          
-  password: "Passion1218!",          
-  database: "Homegoods" 
+  host: DB_HOST,
+  user: DB_USER,  
+  port: DB_PORT,        
+  password: DB_PASSWORD,          
+  database: DB_DATABASE 
 });
 
 export default db;
